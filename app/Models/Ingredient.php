35440ddc582b30e375c3e0ec249c3fa93b525ee7
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-26 16:39:58 +0000

    pivot data was returning variables that already appear. created  to append the correct pivot data

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ingredient extends Model
 {
@@ -12,10 +13,24 @@ class Ingredient extends Model
 
     protected $fillable = ['name', 'food_group', 'allergen'];
 
-    protected $hidden = ['created_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at', 'pivot'];
 
-    public function recipes()
+    protected $appends = ['pivot_data'];
+
+    public function recipes(): BelongsToMany
     {
         return $this->belongsToMany(Recipe::class)->withPivot('quantity', 'unit');
+    }
+
+    public function getPivotDataAttribute(): array|null
+    {
+        if(!$this->pivot){
+            return null;
+        }
+
+        return [
+            'quantity' => $this->pivot->quantity,
+            'unit' => $this->pivot->unit,
+        ];
     }
 }
