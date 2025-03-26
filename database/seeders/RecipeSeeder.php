b5b0dfbf49facfd13f9131194839b2b50c127e28
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-26 15:47:56 +0000

    mirations, models, factories, seeders setup

---
+++ database/seeders/RecipeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RecipeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('recipes')->insert([
+                'name' => fake()->words(2, true),
+                'description' => fake()->words(20, true),
+                'image' => 'https://placehold.co/600x400',
+                'cooking_time' => fake()->time('i'),
+                'serves' => rand(1, 4),
+                'user_id' => rand(1, 2),
+            ]);
+        }
+    }
+}
