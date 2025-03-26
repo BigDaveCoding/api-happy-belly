d423d986a8d9c6cb6f2de014866dd5539a52acc9
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-26 16:18:11 +0000

    composer pint

--- app/Http/Controllers/RecipeApiController.php
+++ app/Http/Controllers/RecipeApiController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Recipe;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class RecipeApiController extends Controller
 {
@@ -13,8 +12,8 @@ class RecipeApiController extends Controller
         $recipeData = Recipe::all()->makeHidden(['description', 'user_id']);
 
         return response()->json([
-           'message' => 'Recipes found successfully',
-           'data' => $recipeData
-        ]);
+            'message' => 'Recipes found successfully',
+            'data' => $recipeData,
+        ], 200);
     }
 }
---
+++ tests/Feature/RecipeApiControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Recipe;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class RecipeApiControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_recipe_api_controller_all_recipes_returned_successfully(): void
+    {
+        Recipe::factory()->count(5)->create();
+        $response = $this->get('/api/recipes');
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $response) {
+                $response->hasAll('message', 'data')
+                    ->whereAllType([
+                        'message' => 'string',
+                        'data' => 'array',
+                    ])
+                    ->has('data', 5, function (AssertableJson $data) {
+                        $data->hasAll('id', 'name', 'image', 'cooking_time', 'serves')
+                            ->whereAllType([
+                                'id' => 'integer',
+                                'name' => 'string',
+                                'image' => 'string',
+                                'cooking_time' => 'integer',
+                                'serves' => 'integer',
+                            ]);
+                    });
+            });
+    }
+}
