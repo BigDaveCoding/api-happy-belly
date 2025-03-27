327cb55aa76694d502c3d00e3831c4e211e63564
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-27 12:20:27 +0000

    test for successful ingredients added to table

--- tests/Feature/RecipeApiServiceProviderTest.php
+++ tests/Feature/RecipeApiServiceProviderTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\Recipe;
 use App\Models\User;
 use App\Providers\RecipeApiServiceProvider;
-use ErrorException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -52,5 +51,36 @@ class RecipeApiServiceProviderTest extends TestCase
         ];
         $this->expectException(QueryException::class);
         $response = RecipeApiServiceProvider::createRecipe($recipeData, 1);
+    }
+
+    public function test_add_ingredients_adds_ingredients_to_database(): void
+    {
+        $user = User::factory()->create(['id' => 1]);
+
+        $recipe = Recipe::factory()->create([
+            'id' => 1,
+            'name' => 'pasta',
+            'description' => 'Description',
+            'cooking_time' => 30,
+            'serves' => 2,
+            'cuisine' => 'italian',
+            'user_id' => $user->id,
+        ]);
+
+        $ingredientData = [
+            'ingredient_name' => ['ingredient', 'peas', 'null'],
+            'ingredient_quantity' => [1, 2, 2],
+            'ingredient_unit' => ['grams', 'cups', null],
+            'ingredient_allergen' => [0, 1, 0],
+        ];
+
+        RecipeApiServiceProvider::addIngredients($ingredientData, $recipe);
+
+        $this->assertDatabaseHas('ingredients', ['name' => 'ingredient', 'food_group' => 'food_group', 'allergen' => 0]);
+        $this->assertDatabaseHas('ingredients', ['name' => 'peas', 'food_group' => 'food_group', 'allergen' => 1]);
+        $this->assertDatabaseHas('ingredients', ['name' => 'null', 'food_group' => 'food_group', 'allergen' => 0]);
+        $this->assertDatabaseHas('ingredient_recipe', ['recipe_id' => 1, 'ingredient_id' => 1, 'quantity' => 1, 'unit' => 'grams']);
+        $this->assertDatabaseHas('ingredient_recipe', ['recipe_id' => 1, 'ingredient_id' => 2, 'quantity' => 2, 'unit' => 'cups']);
+        $this->assertDatabaseHas('ingredient_recipe', ['recipe_id' => 1, 'ingredient_id' => 3, 'quantity' => 2, 'unit' => null]);
     }
 }
