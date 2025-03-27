1890d6afa25534f96431f280937f4b6ebcbbed91
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-27 17:15:41 +0000

    renamed request as I will use it to validate editing recipe aswell

--- app/Http/Requests/RecipeRequest.php
+++ app/Http/Requests/RecipeRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateRecipeRequest extends FormRequest
+class RecipeRequest extends FormRequest
 {
     public function rules(): array
     {
