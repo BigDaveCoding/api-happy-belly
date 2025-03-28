c7ca33e74c01e3efbe6817225971b4ca619cecdc
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-28 14:20:32 +0000

    method which logs user in or returns validation error / unathorised error if credentials dont match

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function login(Request $request): JsonResponse
+    public function login(LoginRequest $request): JsonResponse
     {
-        // validate request data
+        // validate login details
+        $validatedLogin = $request->validated();
 
-        //
+        // Find user by email
+        $user = User::where('email', $validatedLogin['email'])->first();
 
+        // check password in request matches users password
+        // if not a match then returns login failed
+        if (!Hash::check($validatedLogin['password'], $user->password)) {
+            return response()->json([
+                'message' => 'login failed',
+            ], 401);
+        }
+
+        // Generate token
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        // Return token in the response
         return response()->json([
-            'message' => 'login successful',
+            'message' => 'Login successful',
+            'token' => $token,
         ]);
     }
 }
