db36fbb3102fb6dac9827a3eac3e96768294a28c
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-30 23:26:26 +0100

    working on routes for email verification

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::controller(RecipeApiController::class)->group(function () {
         Route::get('/recipes', 'all');
         Route::get('/recipes/admin', 'admin');
@@ -28,14 +28,25 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
+// this route is for using the verified middleware.
+// if the user has not verified their email address
+// they will be directed to or sent this response
+
+// I think this doesn't work with api routes??
 Route::get('/email/verify', function () {
     return response()->json([
-        'message' => 'verification link has been sent to your email address.',
+        'message' => 'you have not verified your email',
     ]);
 })->name('verification.notice');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
 
+
+// this route handles the verification
+// when the user clicks the verification link they will be sent the response
+// at the moment it's a json response
+// I could set up a blade template
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill(); // Marks email as verified
     return response()->json(['message' => 'Email verified successfully']);
-})->middleware([ 'signed'])->name('verification.verify');
+})->middleware(['auth:sanctum', 'signed', 'throttle:6, 1'])->name('verification.verify');
