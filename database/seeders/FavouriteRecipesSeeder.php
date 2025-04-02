ff443aa3f66e2bd4d8d30410c3a18cf12803f5cc
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-02 17:21:10 +0100

    pivot table setup and seeder for fake data

--- database/seeders/FavouriteRecipesSeeder.php
+++ database/seeders/FavouriteRecipesSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class RecipeUserSeeder extends Seeder
+class FavouriteRecipesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -19,7 +19,7 @@ class RecipeUserSeeder extends Seeder
             for ($i = 0; $i < 5; $i++) {
                 $randomIndex = rand(1, 10);
                 if (!in_array($randomIndex, $usedIndex)) {
-                    DB::table('recipe_user')->insert([
+                    DB::table('favourite_recipes')->insert([
                         'user_id' => $user,
                         'recipe_id' => $randomIndex,
                     ]);
