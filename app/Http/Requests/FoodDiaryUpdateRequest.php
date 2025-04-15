24bf9d6aa3d9087959156d183600fa167ef3bdf6
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-15 18:41:42 +0100

    moved validation into a request and addedlogic to make sure all arrays for ingredients are the same length if they are provided

---
+++ app/Http/Requests/FoodDiaryUpdateRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FoodDiaryUpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|integer|exists:users,id',
+            'diary_entry' => 'nullable|string|max:5000',
+            'diary_meal_type' => 'nullable|string',
+            'diary_date' => 'nullable|date',
+            'diary_time' => 'nullable|string',
+            'diary_ingredient_name' => 'nullable|array',
+            'diary_ingredient_name.*' => 'nullable|string',
+            'diary_ingredient_quantity' => 'nullable|array',
+            'diary_ingredient_quantity.*' => 'nullable|integer',
+            'diary_ingredient_unit' => 'nullable|array',
+            'diary_ingredient_unit.*' => 'nullable|string',
+            'diary_ingredient_allergen' => 'nullable|array',
+            'diary_ingredient_allergen.*' => 'nullable|boolean',
+            'diary_recipes' => 'nullable|array',
+            'diary_recipes.*' => 'nullable|integer|exists:recipes,id',
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $names = $this->input('diary_ingredient_name', []);
+            $quantities = $this->input('diary_ingredient_quantity', []);
+            $units = $this->input('diary_ingredient_unit', []);
+            $allergens = $this->input('diary_ingredient_allergen', []);
+
+            if (is_array($names) && is_array($quantities) && is_array($units) && is_array($allergens)) {
+                $array_lengths = [
+                    count($names),
+                    count($quantities),
+                    count($units),
+                    count($allergens),
+                ];
+                $uniqueLengths = array_unique($array_lengths);
+                if (count($uniqueLengths) !== 0) {
+                    $validator->errors()->add(
+                        'diary_ingredient_arrays',
+                        'Ingredient arrays (name, quantity, unit, allergen) must all have the same length.'
+                    );
+                }
+            }
+        });
+    }
+}
