fa0e588c9859866da60218fdb4af38e154fa5606
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-15 22:49:00 +0100

    making food_group nullable

--- database/migrations/2025_03_26_153410_create_ingredients_table.php
+++ database/migrations/2025_03_26_153410_create_ingredients_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('food_group');
+            $table->string('food_group')->nullable();
             $table->boolean('allergen');
             $table->timestamps();
         });
