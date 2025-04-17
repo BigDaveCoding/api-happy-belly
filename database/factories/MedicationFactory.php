0767bc0819f168130cf05957d8a913ec5da0bd3d
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-17 19:02:16 +0100

    medication factory

--- database/factories/MedicationFactory.php
+++ database/factories/MedicationFactory.php
@@ -17,7 +17,11 @@ class MedicationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->word(),
+            'strength' => fake()->word(),
+            'form' => fake()->word(),
+            'route' => fake()->word(),
+            'notes' => fake()->sentence(5),
         ];
     }
 }
