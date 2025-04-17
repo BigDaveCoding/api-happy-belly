e0d9bd08979f2e4d23fa1bd5bdf7e71a83184f4d
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-17 18:45:50 +0100

    working on medication migration

---
+++ database/migrations/2025_04_17_171121_create_medications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('strength')->nullable()->comment('e.g 500mg');
+            $table->string('form')->nullable()->comment('e.g. tablet, liquid etc.');
+            $table->string('route')->nullable()->comment('e.g. oral, IV');
+            $table->string('notes')->nullable();
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medications');
+    }
+};
