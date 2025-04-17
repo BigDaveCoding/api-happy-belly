d25a5629ba7a33433b9682292bd255d4ba6649fa
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-17 19:38:18 +0100

    setting up many to many relationships in models

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Medication extends Model
 {
     /** @use HasFactory<\Database\Factories\MedicationFactory> */
     use HasFactory;
+
+    public function bowelWellnessTrackers(): BelongsToMany
+    {
+        return $this->belongsToMany(BowelWellnessTracker::class)->withPivot('prescribed', 'taken_at');
+    }
 }
