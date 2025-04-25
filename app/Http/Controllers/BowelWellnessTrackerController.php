9bca48e1395fef6d0d88398bc274404d84890cd7
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-25 20:30:02 +0100

    working on create function for bowel wellness tracker

--- app/Http/Controllers/BowelWellnessTrackerController.php
+++ app/Http/Controllers/BowelWellnessTrackerController.php
@@ -73,4 +73,48 @@ class BowelWellnessTrackerController extends Controller
             'data' => $entry,
         ]);
     }
+
+    public function create(Request $request): JsonResponse
+    {
+        // validate the data
+        $request->validate([
+            'user_id' => 'required|integer|exists:users,id',
+            'date' => 'required|date_format:Y-m-d',
+            'time' => 'required|string',
+            'stool_type' => 'required|integer|min:1:max:7',
+            'urgency' => 'nullable|integer|min:1|max:10',
+            'pain' => 'nullable|integer|min:1|max:10',
+            'blood' => 'nullable|boolean|min:1|max:10',
+            'blood_amount' => 'nullable|integer|min:1|max:10000',
+            'stress_level' => 'nullable|integer|min:1|max:10',
+            'hydration_level' => 'nullable|integer|min:1|max:10',
+            'recent_meal' => 'nullable|boolean',
+            'color' => 'nullable|string',
+            'additional_notes' => 'nullable|string|max:65535',
+            'medications' => 'sometimes|array',
+            'medications.*' => 'string|max:100'
+        ]);
+
+        // create new entry
+        $entry = new BowelWellnessTracker();
+        // save data to entry
+        $entry->user_id = $request['user_id'];
+        $entry->date = $request['date'];
+        $entry->time = $request['time'];
+        $entry->stool_type = $request['stool_type'];
+        $entry->urgency  = $request['urgency'];
+        $entry->pain = $request['pain'];
+        $entry->blood = $request['blood'];
+        $entry->blood_amount = $request['blood_amount'];
+        $entry->stress_level = $request['stress_level'];
+        $entry->hydration_level = $request['hydration_level'];
+        $entry->recent_meal = $request['recent_meal'];
+        $entry->additional_notes = $request['additional_notes'];
+        // save the entry
+        // Add pivot data
+
+        return response()->json([
+            'message' => 'Bowel Wellness Tracker entry created successfully',
+        ]);
+    }
 }
