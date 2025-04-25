f695dbd3c7cbdb2b98ebe527a54ccb8559184476
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-25 21:03:27 +0100

    create function is creating bwt entry, medication if provided and pivot table data inserted correctly

--- app/Http/Controllers/BowelWellnessTrackerController.php
+++ app/Http/Controllers/BowelWellnessTrackerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BowelWellnessTracker;
+use App\Models\Medication;
 use App\Models\User;
 use App\Providers\PaginationServiceProvider;
 use Illuminate\Http\JsonResponse;
@@ -80,19 +81,31 @@ class BowelWellnessTrackerController extends Controller
         $request->validate([
             'user_id' => 'required|integer|exists:users,id',
             'date' => 'required|date_format:Y-m-d',
-            'time' => 'required|string',
-            'stool_type' => 'required|integer|min:1:max:7',
+            'time' => 'required|date_format:H:i',
+            'stool_type' => 'required|integer|min:1|max:7',
             'urgency' => 'nullable|integer|min:1|max:10',
             'pain' => 'nullable|integer|min:1|max:10',
-            'blood' => 'nullable|boolean|min:1|max:10',
+            'blood' => 'nullable|boolean',
             'blood_amount' => 'nullable|integer|min:1|max:10000',
             'stress_level' => 'nullable|integer|min:1|max:10',
             'hydration_level' => 'nullable|integer|min:1|max:10',
             'recent_meal' => 'nullable|boolean',
             'color' => 'nullable|string',
             'additional_notes' => 'nullable|string|max:65535',
-            'medications' => 'sometimes|array',
-            'medications.*' => 'string|max:100'
+            'medication_name' => 'sometimes|array',
+            'medication_name.*' => 'string|max:100',
+            'medication_strength' => 'sometimes|array',
+            'medication_strength.*' => 'nullable|string|max:100',
+            'medication_form' => 'sometimes|array',
+            'medication_form.*' => 'nullable|string|max:100',
+            'medication_route' => 'sometimes|array',
+            'medication_route.*' => 'nullable|string|max:100',
+            'medication_notes' => 'sometimes|array',
+            'medication_notes.*' => 'nullable|string|max:10000',
+            'medication_prescribed' => 'sometimes|array',
+            'medication_prescribed.*' => 'nullable|boolean|',
+            'medication_taken_at' => 'sometimes|array',
+            'medication_taken_at.*' => 'nullable|string',
         ]);
 
         // create new entry
@@ -109,9 +122,27 @@ class BowelWellnessTrackerController extends Controller
         $entry->stress_level = $request['stress_level'];
         $entry->hydration_level = $request['hydration_level'];
         $entry->recent_meal = $request['recent_meal'];
+        $entry->color = $request['color'];
         $entry->additional_notes = $request['additional_notes'];
         // save the entry
+        $entry->save();
         // Add pivot data
+
+        if (isset($request['medication_name'])) {
+            foreach ($request['medication_name'] as $index => $medication) {
+                $med = Medication::firstOrCreate([
+                    'name' => $medication,
+                    'strength' => $request['medication_strength'][$index],
+                    'form' => $request['medication_form'][$index],
+                    'route' => $request['medication_route'][$index],
+                    'notes' => $request['medication_notes'][$index],
+                ]);
+                $entry->medications()->attach($med, [
+                    'prescribed' => $request['medication_prescribed'][$index],
+                    'taken_at' => $request['medication_taken_at'][$index],
+                ]);
+            }
+        }
 
         return response()->json([
             'message' => 'Bowel Wellness Tracker entry created successfully',
