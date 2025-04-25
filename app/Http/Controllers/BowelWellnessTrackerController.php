5b4cbbe0024bed672d0c5204ccec8217f3a8b610
BigDaveCoding <bigdavecoding@gmail.com>
2025-04-25 21:46:00 +0100

    created a service to handle each part of the create function. calling on functions in service for create function

--- app/Http/Controllers/BowelWellnessTrackerController.php
+++ app/Http/Controllers/BowelWellnessTrackerController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\BowelWellnessTrackerCreateRequest;
 use App\Models\BowelWellnessTracker;
 use App\Models\Medication;
 use App\Models\User;
+use App\Providers\BowelWellnessTrackerService;
 use App\Providers\PaginationServiceProvider;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -78,39 +79,8 @@ class BowelWellnessTrackerController extends Controller
 
     public function create(BowelWellnessTrackerCreateRequest $request): JsonResponse
     {
-        $entry = new BowelWellnessTracker();
-
-        $entry->user_id = $request['user_id'];
-        $entry->date = $request['date'];
-        $entry->time = $request['time'];
-        $entry->stool_type = $request['stool_type'];
-        $entry->urgency  = $request['urgency'];
-        $entry->pain = $request['pain'];
-        $entry->blood = $request['blood'];
-        $entry->blood_amount = $request['blood_amount'];
-        $entry->stress_level = $request['stress_level'];
-        $entry->hydration_level = $request['hydration_level'];
-        $entry->recent_meal = $request['recent_meal'];
-        $entry->color = $request['color'];
-        $entry->additional_notes = $request['additional_notes'];
-
-        $entry->save();
-
-        if (isset($request['medication_name'])) {
-            foreach ($request['medication_name'] as $index => $medication) {
-                $med = Medication::firstOrCreate([
-                    'name' => $medication,
-                    'strength' => $request['medication_strength'][$index],
-                    'form' => $request['medication_form'][$index],
-                    'route' => $request['medication_route'][$index],
-                    'notes' => $request['medication_notes'][$index],
-                ]);
-                $entry->medications()->attach($med, [
-                    'prescribed' => $request['medication_prescribed'][$index],
-                    'taken_at' => $request['medication_taken_at'][$index],
-                ]);
-            }
-        }
+        $entry = BowelWellnessTrackerService::createEntry($request);
+        BowelWellnessTrackerService::medicationPivotData($request, $entry);
 
         return response()->json([
             'message' => 'Bowel Wellness Tracker entry created successfully',
---
+++ app/Providers/BowelWellnessTrackerService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Requests\BowelWellnessTrackerCreateRequest;
+use App\Models\BowelWellnessTracker;
+use App\Models\Medication;
+use Illuminate\Support\ServiceProvider;
+
+class BowelWellnessTrackerService extends ServiceProvider
+{
+    static public function createEntry(BowelWellnessTrackerCreateRequest $request): BowelWellnessTracker
+    {
+        $entry = new BowelWellnessTracker();
+
+        $entry->user_id = $request['user_id'];
+        $entry->date = $request['date'];
+        $entry->time = $request['time'];
+        $entry->stool_type = $request['stool_type'];
+        $entry->urgency  = $request['urgency'];
+        $entry->pain = $request['pain'];
+        $entry->blood = $request['blood'];
+        $entry->blood_amount = $request['blood_amount'];
+        $entry->stress_level = $request['stress_level'];
+        $entry->hydration_level = $request['hydration_level'];
+        $entry->recent_meal = $request['recent_meal'];
+        $entry->color = $request['color'];
+        $entry->additional_notes = $request['additional_notes'];
+
+        $entry->save();
+
+        return $entry;
+    }
+
+    static public function medicationPivotData(BowelWellnessTrackerCreateRequest $request, BowelWellnessTracker $entry)
+    {
+        if (isset($request['medication_name'])) {
+            foreach ($request['medication_name'] as $index => $medication) {
+                $med = Medication::firstOrCreate([
+                    'name' => $medication,
+                    'strength' => $request['medication_strength'][$index],
+                    'form' => $request['medication_form'][$index],
+                    'route' => $request['medication_route'][$index],
+                    'notes' => $request['medication_notes'][$index],
+                ]);
+                $entry->medications()->attach($med, [
+                    'prescribed' => $request['medication_prescribed'][$index],
+                    'taken_at' => $request['medication_taken_at'][$index],
+                ]);
+            }
+        }
+    }
+}
