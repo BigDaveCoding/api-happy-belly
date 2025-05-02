fb9f099284f1b4c5394e8d2dc2e8bc8f539dac75
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-02 01:56:46 +0100

    added custom validation to check arrays are the same length

--- app/Http/Requests/BowelWellnessTrackerUpdateMedicationPivotRequest.php
+++ app/Http/Requests/BowelWellnessTrackerUpdateMedicationPivotRequest.php
@@ -18,4 +18,23 @@ class BowelWellnessTrackerUpdateMedicationPivotRequest extends FormRequest
             'medication_taken_at.*' => 'nullable|string',
         ];
     }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            // creating arrays of passed in data
+            $id = $this->medication_id;
+            $prescribed = $this->medication_prescribed;
+            $taken_at = $this->medication_taken_at;
+
+            // if they're arrays
+            if(is_array($id) && is_array($prescribed) && is_array($taken_at)){
+                $lenghts = [count($id), count($prescribed), count($taken_at)];
+                $unique_lengths = array_unique($lenghts);
+                if (count($unique_lengths) > 1){
+                    $validator->errors()->add('id, prescribed, taken_at', 'Length of each array must be the same (can include null for prescribed|taken_at)');
+                }
+           }
+        });
+    }
 }
